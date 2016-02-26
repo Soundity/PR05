a8e821c5e655b60702b673c0218ddf5158774765
carlos93263 <93263.joan23@fje.edu>
2016-02-26 12:25:28 +0100

    Funcionalidad de Registro i registro PRoc CArlos

--- procs/registro.proc.php
+++ procs/registro.proc.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
-	if (isset($_SESSION[''])){
+	if (isset($_SESSION['mail'])){
 		//MODIFICAR AQUI ->
-		header("location: PAGINA DONDE VAYA");
+		header("location: index.php");
 	}else{
 		//Incluimos la conexion a BBDD
 		include ("../conexion.php");
@@ -13,14 +13,28 @@
 		//Lanzamiento de la consulta
 		$sql = "INSERT INTO `bd_soundity`.`tbl_usuari` (`usu_id`, `usu_mail`, `usu_contra`, `usu_nom`, `usu_avatar`, `usu_descripcio`, `usu_rang`, `usu_idioma`) VALUES (NULL, '$correo','$contrasena', '$apodo', NULL, NULL, '0', NULL)";
 		$datos = mysqli_query($con,$sql);
-
 		//COMPROVACIONES VARIAS
 		//si todo sale bien
 		if (mysqli_affected_rows($con) == 1){
-			//MODIFICAR AQUI ->
-			header("location: ../login.php");
-			$_SESSION['creado_correctamente'] = "El usuario se ha creado correctamente, bienvenido a nuestra comunidad.";
-		//en caso de error
+			if (isset($_REQUEST["gustos"])){
+				$gustos = $_REQUEST['gustos'];
+				$sql2= "SELECT `usu_id` FROM `tbl_usuari` WHERE `usu_mail`='$correo'";
+			    $datos2 = mysqli_query($con,$sql2);
+			    while($valor2=mysqli_fetch_array($datos2)){
+					$id_usuario = $valor2['usu_id'];
+				}
+
+				for($i=0; $i<count($gustos); $i++) {
+				    $gusto = $gustos[$i];
+				    $sql3 = "INSERT INTO `bd_soundity`.`tbl_genere_usuari` (`gus_id`, `usu_id`, `gen_id`) VALUES (NULL, '$id_usuario', '$gusto')";
+				    $datos3 = mysqli_query($con,$sql3);
+				}
+				header("location: ../login.php");
+				$_SESSION['creado_correctamente'] = "El usuario se ha creado correctamente, bienvenido a nuestra comunidad.";
+			} else {
+				header("location: ../login.php");
+				$_SESSION['creado_correctamente'] = "El usuario se ha creado correctamente, bienvenido a nuestra comunidad.";
+			}
 		}else{
 			header("location: ../registro.php");
 			//$_SESSION['fallo_registro'] = "Ha habido un error con el registro, intentalo de nuevo por favor."
--- registro.php
+++ registro.php
@@ -1,6 +1,9 @@
 <?php
-
 	session_start();
+	include("conexion.php");
+	$sql = "SELECT * FROM `tbl_genere`";
+	$datos = mysqli_query($con,$sql);
+	$cont = 1;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -8,47 +11,65 @@
     <title>Soundity</title>
     <script src="js/validaFormulario.js" type="text/javascript"></script>
     <link rel="stylesheet" type="text/css" href="dist/semantic.min.css">
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="css/registro.css">
     <meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
 	<body>
 		<div>
 			<!--<label><?php //if(isset($_SESSION['fallo_registro'])) echo $_SESSION['fallo_registro']; session_destroy();?></label>-->
-			<h1><a>Datos personales</a></h1>
-			<form id="form_registro" method="GET" action="procs/registro.proc.php" onsubmit="return validaFormulario();">
+			
+			<form id="form_registro" class="ui center aligned orange segment" method="GET" action="procs/registro.proc.php" onsubmit="return validaFormulario();">
+				<h2 class="ui header">Registro</h2>
+				<div class="ui form">
 				<!-- CORREO ELECTRONICO DEL USUARIO -->
-				<label>Correo Electrónico: </label>
-				<span>
-					<input id="correo" name="correo" class="element text medium" type="text" maxlength="50" size="30" value=""/>
-					<span id="error_correo_vacio"></span>
-					<span id="error_correo_formato"></span>
-				</span>
-				<br/><br/>
-				<!-- CONTRASEÑA DEL USUARIO -->
-				<label>Contraseña:</label>
-				<span>
-					<input id="contrasena" name="contrasena" type="password" value=""/>
-					<span id="error_contrasena_vacio"></span>
-				</span>
-				<br/><br/>
-				<!-- CONFIRMACIÓN DEL USUARIO -->
-				<label>Confirmar contraseña:</label>
-				<span>
-    				<input id="confirmar_contrasena" type="password" />
-    				<span id="error_confirmar_contrasena_vacio"></span>
-    				<span id="error_confirmar_contrasena_incorrecto"></span>
-				</span>
-				<br/><br/>
-				<!-- APODO O NICKNAME DEL USUARIO -->
-				<label>Apodo / Nickname: </label>
-				<span>
-					<input id="apodo" name="apodo"  maxlength="30" size="30" value=""/>
-					<span id="error_apodo"></span>
-				</span>
-				<br/><br/>
-				<!-- BOTON DE ENVIAR -->
-				<input type="submit" name="submit" value="Enviar" />
+					<label>Correo Electrónico: </label>
+					<div class="five wide field">
+						<input id="correo" name="correo" class="element text medium" type="text" maxlength="50" size="30" value=""/>
+						<span id="error_correo_vacio"></span>
+						<span id="error_correo_formato"></span>
+					</div>
+					<!-- CONTRASEÑA DEL USUARIO -->
+					<label>Contraseña:</label>
+					<div class="five wide field">
+						<input id="contrasena" name="contrasena" type="password" value=""/>
+						<span id="error_contrasena_vacio"></span>
+					</div>
+					<!-- CONFIRMACIÓN DEL USUARIO -->
+					<label>Confirmar contraseña:</label>
+					<div class="five wide field">
+	    				<input id="confirmar_contrasena" type="password" />
+	    				<span id="error_confirmar_contrasena_vacio"></span>
+	    				<span id="error_confirmar_contrasena_incorrecto"></span>
+					</div>
+					<!-- APODO O NICKNAME DEL USUARIO -->
+					<label>Apodo / Nickname: </label>
+					<div class="five wide field">
+						<input id="apodo" name="apodo"  maxlength="30" size="30" value=""/>
+						<span id="error_apodo"></span>
+					</div>
+					<!-- CHECKBOX DE GUSTOS -->
+					<label>Selecciona tus gustos: </label>
+					<div class="five wide field">
+						<?php 
+						while($valor=mysqli_fetch_array($datos)){
+							echo "<div class='ui checkbox'><input type='checkbox' name='gustos[]' value='".$valor['gen_id']."'/><label>".$valor['gen_nom']."</label></div>";     
+							
+							/*if ($cont == 5){
+								$cont=1;
+								echo "<br/>";
+							} else {
+								$cont++;
+							}*/
+						}
+						?>
+						<label>Mas adelante podras elegirlos y/o modificarlos.</label>
+					</div>
+					
+					<!-- BOTON DE ENVIAR -->
+					<input type="submit" class="ui inverted orange button" name="submit" value="Registrarse" />
+					 <input type="button" class="ui inverted orange button" onclick="location='Login.php'" value="Atrás"/>
+				</div>
 			</form>
 		</div>
 	</body>
