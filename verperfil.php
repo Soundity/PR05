df62998fdc0a564e263dc30069bb0ffa19e09db1
sergioayalaj23 <39202.joan23@fje.edu>
2016-02-26 12:14:59 +0100

    Web Perfil

---
+++ verperfil.php
@@ -0,0 +1,187 @@
+<?php
+session_start();
+include('header_menu.html');
+include('conexion.php');
+  if(isset($_SESSION['id']))$login = 1;
+  if(isset($_COOKIE['Soundity']))$login = 1;
+  if($login == 1){
+      if (isset($_REQUEST['iduser'])){
+          $sql="SELECT * FROM tbl_usuari LEFT JOIN tbl_musica ON tbl_usuari.usu_id=tbl_musica.usu_id WHERE tbl_usuari.usu_id=$_REQUEST[iduser]";
+          $datos = mysqli_query($con, $sql);
+
+        ?>
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <title>Soundity</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/generos.css">
+        <link rel="stylesheet" type="text/css" href="css/styleRep1.css">
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>
+        <script type="text/javascript" src="js/botonesReproductor1.js"></script>
+        </head>
+            <body>
+            
+                <?php
+                $contador=0;
+                
+                while($perfil = mysqli_fetch_array($datos)) {
+                    
+                  if ($contador==0){
+                
+                    echo utf8_encode($perfil['usu_nom']);
+                    echo "</br>";
+                    echo utf8_encode($perfil['usu_descripcio']);
+
+                    ?>
+                    <section id="player" data-autoplay='1' data-loop='1'>
+      <section id="controls">
+        <section id="songTitle">
+          <span>Selecciona una canción</span>
+        </section>
+        <section id="playertrols">
+          <div id="plauseStop">
+            <div id="plause"></div>
+            <div id="stop"></div>
+          </div>
+          <div id="progressBar">
+            <div id="timeLoaded"></div>
+            <div id="timePlayed"></div>
+          </div>
+        </section>
+        <section id="volumeTime">
+          <input type="range" min="0" max="1" step="0.1" value="0.5" />
+          <div id="timeStatus">
+            <time id="played">00:00</time>
+            <span>/</span>
+            <time id="totalTime">00:00</time>
+          </div>
+        </section>
+      </section>
+    </section>
+  </section>
+</section>
+
+<?php
+                    $contador=$contador+1;
+                  }
+                    echo "</br>";
+                    if(isset($perfil['mus_titol'])){
+                   ?>
+          <article class="cancion" data-source="media/music/<?php echo $perfil['mus_nom'] ?>">
+
+            <p><?php 
+            echo "Nombre: ".$perfil['mus_titol']."   |   Autor: ".$perfil['usu_nom']; ?></p>
+          </article>
+          
+<?php
+     }         }
+           
+                ?>
+
+              
+            </body>
+        </html>
+
+
+
+          
+          <?php
+      }elseif(isset($_SESSION['id'])){
+          $sql="SELECT * FROM tbl_usuari LEFT JOIN tbl_musica ON tbl_usuari.usu_id=tbl_musica.usu_id WHERE tbl_usuari.usu_id=$_SESSION[id]";
+          
+          $datos = mysqli_query($con, $sql);
+          ?>
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <title>Soundity</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/generos.css">
+        <link rel="stylesheet" type="text/css" href="css/styleRep1.css">
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>
+        <script type="text/javascript" src="js/botonesReproductor1.js"></script>
+        </head>
+            <body>
+           
+                <?php
+                $contador=0;
+                
+                while($perfil = mysqli_fetch_array($datos)) {
+                  
+                  if ($contador==0){
+             
+
+                    echo utf8_encode($perfil['usu_nom']);
+                    echo "</br>";
+                    echo utf8_encode($perfil['usu_descripcio']);
+
+                                  ?>
+                    <section id="player" data-autoplay='1' data-loop='1'>
+      <section id="controls">
+        <section id="songTitle">
+          <span>Selecciona una canción</span>
+        </section>
+        <section id="playertrols">
+          <div id="plauseStop">
+            <div id="plause"></div>
+            <div id="stop"></div>
+          </div>
+          <div id="progressBar">
+            <div id="timeLoaded"></div>
+            <div id="timePlayed"></div>
+          </div>
+        </section>
+        <section id="volumeTime">
+          <input type="range" min="0" max="1" step="0.1" value="0.5" />
+          <div id="timeStatus">
+            <time id="played">00:00</time>
+            <span>/</span>
+            <time id="totalTime">00:00</time>
+          </div>
+        </section>
+      </section>
+    </section>
+  </section>
+</section>
+
+<?php
+                    $contador=$contador+1;
+                  }
+                    echo "</br>";
+                    if(isset($perfil['mus_titol'])){
+                   ?>
+          <article class="cancion" data-source="media/music/<?php echo $perfil['mus_nom'] ?>">
+
+            <p><?php 
+            echo "Nombre: ".$perfil['mus_titol']."   |   Autor: ".$perfil['usu_nom']; ?></p>
+          </article>
+          
+<?php
+     }         }
+                 
+                ?>
+
+              
+            </body>
+        </html>
+
+
+
+
+
+
+          
+          <?php
+      }else{
+    header("Location: index.php");
+      }
+
+  }else{
+    $_SESSION['validarse'] = 'error';
+    header("Location: login.php");
+    die();
+  }
+?>
