6c1276898062c0d474f9e3daf1ab3e9a090a7937
victorcruz94 <10000316.joan23@fje.edu>
2016-02-26 13:04:47 +0100

    busqueda con reproductor

--- busqueda.php
+++ busqueda.php
@@ -14,6 +14,10 @@ include('conexion.php');
 		<title>¡web de resultados!</title>
 		<link rel="stylesheet" href="dist/semantic.min.css">
 		<link rel="stylesheet" href="css/busqueda.css">
+		<link rel="stylesheet" type="text/css" href="css/styleRep1.css">
+		<script type="text/javascript" src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+		<script src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>
+		<script type="text/javascript" src="js/botonesReproductor1.js"></script>
 	</head>
 	<body>
 		<div class="ui grid">
@@ -28,7 +32,7 @@ include('conexion.php');
 				echo "</div>";
 			}else{
 				while($pro0 = mysqli_fetch_array($datos0)) {
-					echo "<div class='seven wide centered column'>";
+					echo "<div class='twelve wide centered column'>";
 					echo "<div class='ui orange raised segment'>";
 					echo "<div class='ui horizontal divider'>";
 					echo utf8_encode("<h2>$pro0[usu_nom]</h2>");
@@ -70,11 +74,36 @@ include('conexion.php');
   									<i class='thumbs up large icon'></i> 23
 								</div>";
 						echo "</div>";
+						?><article class="cancion" data-source="media/music/<?php echo $pro1['mus_nom']; ?>"><p id="nomOculto"><?php echo $pro1['mus_titol']; ?></p><img src="media/images/play.png"></img></article><?php
 						echo "</div>";
 						echo "</div>";
 					}
 				}
 			?>
   		</div> 
+  		<section id="player" data-autoplay='1' data-loop='1'>
+			<section id="controls">
+				<section id="songTitle">
+					<span>Selecciona una canción</span>
+				</section>
+				<section id="playertrols">
+					<div id="plauseStop">
+						<div id="plause"></div>
+						<div id="stop"></div>
+					</div>
+					<div id="progressBar">
+						<div id="timeLoaded"><div id="timePlayed"></div></div>
+					</div>
+				</section>
+				<section id="volumeTime">
+					<input type="range" min="0" max="1" step="0.1" value="0.5" />
+					<div id="timeStatus">
+						<time id="played">00:00</time>
+						<span>/</span>
+						<time id="totalTime">00:00</time>
+					</div>
+				</section>
+			</section>
+		</section>
 	</body>
 </html>
