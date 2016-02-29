d804b7806fe37fe44fd0722b3dd3e1be59192032
carlos93263 <93263.joan23@fje.edu>
2016-02-29 13:07:18 +0100

    modifcar.php i proc funcionando correctamente

--- modificar.php
+++ modificar.php
@@ -3,82 +3,98 @@
 	if(isset($_SESSION['id']))$login = 1;
 	if(isset($_COOKIE['Soundity']))$login = 1;
 	if($login == 1){
-
-include("conexion.php");
+	include("conexion.php");
+	$sql2 = "SELECT * FROM `tbl_genere`";
+	$datos2 = mysqli_query($con,$sql2);
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8" />
 		<title>Modificar usuario</title>   
-        <script type="text/javascript" src="/jsvalidaFormulario.js"></script>
+        <script type="text/javascript" src="js/validaFormulario.js"></script>
 	</head>
 	<body>
 		<?php include('header_menu.html'); 
-			$sql = "SELECT * FROM tbl_usuari WHERE usu_id=". $_SESSION['id'];
+			//$sql = "SELECT * FROM tbl_usuari WHERE usu_id=". $_SESSION['id'];
+			$sql ="SELECT * FROM `tbl_usuari` WHERE `tbl_usuari`.`usu_id`=".$_SESSION['id'];
 			//mostramos la consulta para ver por pantalla si es lo que esperábamos o no
 			//echo "$sql<br/>";
 			//lanzamos la sentencia sql que devuelve el producto en cuestión
 			$datos = mysqli_query($con, $sql);
 			if(mysqli_num_rows($datos)>0){
 				$prod=mysqli_fetch_array($datos);
-?>
-		<form id="form_registro" method="GET" action="procs/modificar.proc.php" onsubmit="return validaFormulario();" enctype="multipart/form-data">
-			<!-- CORREO ELECTRONICO DEL USUARIO -->
-			<label>Correo Electrónico: </label>
-			<span>
-				<input id="correo" name="correo" class="element text medium" type="text" maxlength="50" size="30" value="<?php echo $prod['usu_mail']; ?>"/>
-				<span id="error_correo_vacio"></span>
-				<span id="error_correo_formato"></span>
-			</span>
-			<br/><br/>
-			<!-- CONTRASEÑA DEL USUARIO -->
-			<label>Contraseña:</label>
-			<span>
-				<input id="contrasena" name="contrasena" maxlength="50" type="password" value="<?php echo $prod['usu_contra']; ?>"/>
-				<span id="error_contrasena_vacio"></span>
-			</span>
-			<br/><br/>
-			<!-- CONFIRMACIÓN DEL USUARIO -->
-			<label>Confirmar contraseña:</label>
-			<span>
-				<input id="confirmar_contrasena" maxlength="50" type="password" value="<?php echo $prod['usu_contra']; ?>"/>
-				<span id="error_confirmar_contrasena_vacio"></span>
-				<span id="error_confirmar_contrasena_incorrecto"></span>
-			</span>
-			<br/><br/>
-			Imagen:<br/>
-			<input type="file" name="imagen" id="imagen"/><br/>
-			<!-- APODO O NICKNAME DEL USUARIO -->
-			<label>Apodo / Nickname: </label>
-			<span>
-				<input id="apodo" name="apodo"  maxlength="30" size="30" value="<?php echo $prod['usu_nom']; ?>"/>
-				<span id="error_apodo"></span>
-			</span>
-			<br/><br/>
-			Descripcion : 
-				<textarea name="descripcion" rows="4" cols="50" maxlength="250">
-					<?php echo $prod['usu_descripcio'];?>
-				</textarea>
-			Idioma : 
-				<input id="idioma" name= "idioma" class="element text" maxlength="20" size="48" value="<?php echo utf8_encode($prod['usu_idioma']); ?>"/><br/>
-			<!-- BOTON DE ENVIAR -->
-			<input type="submit" name="submit" value="Enviar" />
-			<?php
-				} else {
-					//echo "Usuario con id=$_REQUEST[id_usuario] no encontrado!";
-				}
-				//cerramos la conexión con la base de datos
-				mysqli_close($con);
-			?>
-		</form>
+		?>
+				<form id="form_registro" method="POST" action="procs/modificar.proc.php" onsubmit="return validaFormulario();" enctype="multipart/form-data">
+					<!-- CORREO ELECTRONICO DEL USUARIO -->
+					<label>Correo Electrónico: </label>
+					<span>
+						<input id="correo" name="correo" class="element text medium" type="text" maxlength="50" size="30" value="<?php echo $prod['usu_mail']; ?>"/>
+						<span id="error_correo_vacio"></span>
+						<span id="error_correo_formato"></span>
+					</span>
+					<br/><br/>
+					<!-- CONTRASEÑA DEL USUARIO -->
+					<label>Nueva contraseña:</label>
+					<span>
+						<input id="contrasena" name="contrasena" maxlength="50" type="password"/>
+						<span id="error_contrasena_vacio"></span>
+					</span>
+					<br/><br/>
+					<!-- CONFIRMACIÓN DEL USUARIO -->
+					<label>Confirmar nueva contraseña:</label>
+					<span>
+						<input id="confirmar_contrasena" maxlength="50" type="password"/>
+						<span id="error_confirmar_contrasena_vacio"></span>
+						<span id="error_confirmar_contrasena_incorrecto"></span>
+					</span>
+					<br/><br/>
+					Imagen:<br/>
+					<input type="file" name="imagen" id="imagen"/><br/>
+					<!-- APODO O NICKNAME DEL USUARIO -->
+					<label>Apodo / Nickname: </label>
+					<span>
+						<input id="apodo" name="apodo"  maxlength="30" size="30" value="<?php echo $prod['usu_nom']; ?>"/>
+						<span id="error_apodo"></span>
+					</span>
+					<br/><br/>
+					Descripcion : 
+						<textarea name="descripcion" rows="4" cols="50" maxlength="250">
+							<?php echo $prod['usu_descripcio'];?>
+						</textarea>
+					Idioma : 
+						<input id="idioma" name= "idioma" class="element text" maxlength="20" size="48" value="<?php echo utf8_encode($prod['usu_idioma']); ?>"/><br/>
+					Generos:
+					<?php 
+						while($valor2=mysqli_fetch_array($datos2)){
+							$sql3 = "SELECT `gen_id` FROM `tbl_genere_usuari` WHERE `usu_id`='$_SESSION[id]' AND `gen_id`='$valor2[gen_id]'";
+							//echo $sql3;
+							$datos3 = mysqli_query($con,$sql3);
+							if (mysqli_num_rows($datos3)==1){
+								echo "<div class='ui checkbox'><input type='checkbox' name='gustos[]' value='".$valor2['gen_id']."' checked/><label>".$valor2['gen_nom']."</label></div>";
+							}else{
+								echo "<div class='ui checkbox'><input type='checkbox' name='gustos[]' value='".$valor2['gen_id']."'/><label>".$valor2['gen_nom']."</label></div>";
+							}
+						}
+					?>
+					<br/><br/>
+						<!-- BOTON DE ENVIAR -->
+					<input type="submit" name="submit" value="Enviar" />
+				</form>
+		<?php
+			} else {
+				//echo "Usuario con id=$_REQUEST[id_usuario] no encontrado!";
+			}
+			//cerramos la conexión con la base de datos
+			mysqli_close($con);
+		?>
 		<br/><br/>
 	</body>
 </html>
 <?php
-  }else{
-    $_SESSION['validarse'] = 'error';
-    header("Location: login.php");
-    die();
-  }
+ 	}else{
+	    $_SESSION['validarse'] = 'error';
+	    header("Location: login.php");
+	    die();
+	}
 ?>
--- procs/modificar.proc.php
+++ procs/modificar.proc.php
@@ -1,50 +1,53 @@
-<?php 
- if(isset($_SESSION['id']))$login = 1;
-  if(isset($_COOKIE['Soundity']))$login = 1;
-  if($login == 1){
-
-
- ?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8"/>
-		<title>Modificar update BD</title>
-	</head>
-	<body>
-		<?php
-			$password=md5($_REQUEST['contrasena']);
-			if(empty($_FILES['imagen']['name'])){
-
-
-			$sql = "UPDATE tbl_usuari SET usu_nom='$_REQUEST[apodo]', usu_mail='$_REQUEST[correo]', usu_contra='$password', usu_descripcio='$_REQUEST[descripcion]', usu_idioma='$_REQUEST[idioma]' WHERE usu_id=$_SESSION[id]";
-
+<?php
+	session_start();
+	if(isset($_SESSION['id']))$login = 1;
+	if(isset($_COOKIE['Soundity']))$login = 1;
+	if($login == 1){
+		include("../conexion.php");
+		$password=md5($_REQUEST['contrasena']);
+		$apodo=$_REQUEST['apodo'];
+		$correo=$_REQUEST['correo'];
+		$descrip=$_REQUEST['descripcion'];
+		$idioma=$_REQUEST['idioma'];
+		echo $_FILES['imagen']['name'];
+		if(empty($_FILES['imagen']['name'])){
+			$sql = "UPDATE tbl_usuari SET usu_nom='$apodo', usu_mail='$correo', usu_contra='$password', usu_descripcio='$descrip', usu_idioma='$idioma' WHERE usu_id=$_SESSION[id]";
 			$datos = mysqli_query($con, $sql);
-
-			//header("location: modificaruser.php")
+			header("location: ../modificar.php");
 		}else{
-			$ruta = "img/" . $_FILES['imagen']['name'];
-			$imagen=$_FILES['imagen']['name'];
-
+			$ruta = "../media/images/avatares/".$_SESSION['id']."_".$_FILES['imagen']['name'];
+			$imagen=$_SESSION['id']."_".$_FILES['imagen']['name'];
 			$resultado = @move_uploaded_file($_FILES["imagen"]["tmp_name"], $ruta);
-
-			$sql = "UPDATE tbl_usuari SET usu_nom='$_REQUEST[apodo]', usu_avatar='$imagen', usu_mail='$_REQUEST[correo]', usu_contra='$password', usu_descripcio='$_REQUEST[descripcion]', usu_idioma='$_REQUEST[idioma]' WHERE usu_id=$_SESSION[id]";
-
+			$sql = "UPDATE tbl_usuari SET usu_nom='$apodo', usu_avatar='$imagen', usu_mail='$correo', usu_contra='$password', usu_descripcio='$descrip', usu_idioma='$idioma' WHERE usu_id=$_SESSION[id]";
 			$datos = mysqli_query($con, $sql);
-
-			//header("location: principal.php")
+			header("location: ../modificar.php");
 		}
-		?>
 
-	</body>
-</html>
-
-
-		<?php
-  }else{
-    $_SESSION['validarse'] = 'error';
-    header("Location: login.php");
-    die();
-  }
+		if (mysqli_affected_rows($con) == 1){
+			if (isset($_REQUEST["gustos"])){
+				$gustos = $_REQUEST['gustos'];
+				$sql2= "DELETE FROM `tbl_genere_usuari` WHERE `usu_id`=$_SESSION[id]";
+				$datos2 = mysqli_query($con,$sql2);
+				for($i=0; $i<count($gustos); $i++) {
+				    $gusto = $gustos[$i];
+				    $sql3 = "INSERT INTO `bd_soundity`.`tbl_genere_usuari` (`gus_id`, `usu_id`, `gen_id`) VALUES (NULL, '$_SESSION[id]', '$gusto')";
+				    $datos3 = mysqli_query($con,$sql3);
+				}
+				header("location: ../modificar.php");
+			} else {
+				$sql2= "DELETE FROM `tbl_genere_usuari` WHERE `usu_id`=$_SESSION[id]";
+				$datos2 = mysqli_query($con,$sql2);
+				header("location: ../modificar.php");
+			}
+		}else{
+			header("location: ../modificar.php");
+			//MODIFICAR ESTO PARA QUE SEA UN ERROR DE MODIFICACION, NO DE REGISTRO
+			$_SESSION['fallo_registro'] = "Ha habido un error con el registro, intentalo de nuevo por favor.";
+		}
+		
+	}else{
+		$_SESSION['validarse'] = 'error';
+		header("Location: ../login.php");
+		die();
+	}
 ?>
