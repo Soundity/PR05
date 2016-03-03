770e1c5de639e57ed5dcbd02ce9e5a0d46295e81
EnricGorriz <9074.joan23@fje.edu>
2016-03-03 12:02:27 +0100

    index amb valoració, falta afegir a llistes

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ include('conexion.php');
         <script type="text/javascript" src="//code.jquery.com/jquery-1.12.0.min.js"></script>
         <script src="http://cdn.jquerytools.org/1.2.6/full/jquery.tools.min.js"></script>
         <script type="text/javascript" src="js/botonesReproductor1.js"></script>
+		<script type="text/javascript" src="js/valoracion.js"></script>
 	</head>
 
 	<body>
@@ -58,7 +59,32 @@ include('conexion.php');
 						
 						echo "<h3>Género: </h3><p>$pro1[gen_nom]</p>";
 						echo utf8_encode("<h3>Autor: </h3><p>$pro1[usu_nom]</p>");
-						echo "<h3>Valoración: </h3><div class='ui label'><i class='thumbs up large icon'></i>$pro1[totalvots]</div></div></article></div>";
+						echo "<h3>Valoración: </h3><div class='ui label'>";
+						$usuari=$_SESSION['id'];
+						$sql2 = "Select * from tbl_valoracio where mus_id=$pro1[mus_id] & usu_id=$usuari";
+						//echo $sql2;
+						$datos2 = mysqli_query($con, $sql2);
+						if(mysqli_num_rows($datos2)==0){
+							// POTS VOTAR
+							echo"<i id=$pro1[mus_id] class=' thumbs outline up icon' onclick='suscriM($pro1[mus_id]);'></i>";
+							echo"<i id=$pro1[mus_id] class=' thumbs outline down icon' onclick='suscriN($pro1[mus_id]);'></i>";
+							if ($pro1['totalvots']!=0){
+								echo $pro1['totalvots']. " Votos";
+							} else {
+								echo "0 Votos";
+							}
+						}else{
+							// JA HAS VOTAR 
+							while($pro2 = mysqli_fetch_array($datos2)) {	
+								if ($pro2['val_puntuacio']==1){
+									echo"<i id=$pro1[mus_id] class=' thumbs outline down icon' onclick='suscriN($pro1[mus_id]);'></i>$pro1[totalvots] Te Gusta ";
+								} else {
+									echo"<i id=$pro1[mus_id] class=' thumbs outline up icon' onclick='suscriM($pro1[mus_id]);'></i>$pro1[totalvots] Ya no te gusta ";
+									
+								}
+							}
+						}
+						echo "</div></div></article></div>";
 					}
 					echo "</div>";
 				}
@@ -69,7 +95,7 @@ include('conexion.php');
 			<?php
 			//tbl_genere.gen_nom, tbl_usuari.usu_nom, tbl_musica.mus_titol, tbl_musica.mus_nom, tbl_musica.usu_comptador
 				$id = $_SESSION['id'];
-				$sql0 = "SELECT * FROM tbl_genere_usuari inner join tbl_genere on tbl_genere_usuari.gen_id=tbl_genere.gen_id inner join tbl_musica on tbl_genere.gen_id=tbl_musica.gen_id inner join tbl_usuari on tbl_musica.usu_id=tbl_usuari.usu_id left join (Select sum(tbl_valoracio.val_puntuacio) as totalvots, mus_id from tbl_valoracio group by mus_id) as queryGen on tbl_musica.mus_id=queryGen.mus_id WHERE tbl_genere_usuari.usu_id=$id";
+				$sql0 = "SELECT * FROM tbl_genere_usuari inner join tbl_genere on tbl_genere_usuari.gen_id=tbl_genere.gen_id inner join tbl_musica on tbl_genere.gen_id=tbl_musica.gen_id inner join tbl_usuari on tbl_musica.usu_id=tbl_usuari.usu_id left join (Select sum(tbl_valoracio.val_puntuacio) as totalvots, mus_id as idguai from tbl_valoracio group by 'idguai') as queryGen on tbl_musica.mus_id=queryGen.idguai WHERE tbl_genere_usuari.usu_id=$id";
 				$datos0 = mysqli_query($con, $sql0);
 				if(mysqli_num_rows($datos0)<=0){
 					echo "</br></br><div class='seven wide centered column'>";
@@ -93,10 +119,31 @@ include('conexion.php');
 						
 						echo "<h3>Género: </h3><p>$pro0[gen_nom] </p>";
 						echo utf8_encode("<h3>Autor: </h3><p>$pro0[usu_nom] </p>");
-						echo "<h3>Valoración: </h3>
-								<div class='ui label'>
-  									<i class='thumbs up large icon'></i>$pro0[totalvots]
-								 </div></div></article></div>";
+						echo "<h3>Valoración: </h3><div class='ui label'>";
+						$usuari=$_SESSION['id'];
+						$sql2 = "Select * from tbl_valoracio where mus_id=$pro0[mus_id] & usu_id=$usuari";
+						$datos2 = mysqli_query($con, $sql2);
+						if(mysqli_num_rows($datos2)==0){
+							// POTS VOTAR
+							echo"<i id=$pro0[mus_id] class=' thumbs outline up icon' onclick='suscriM($pro0[mus_id]);'></i>";
+							echo"<i id=$pro0[mus_id] class=' thumbs outline down icon' onclick='suscriN($pro0[mus_id]);'></i>";
+							if ($pro0['totalvots']!=0){
+								echo $pro0['totalvots']. " Votos";
+							} else {
+								echo "0 Votos";
+							}
+						}else{
+							// JA HAS VOTAR 
+							while($pro2 = mysqli_fetch_array($datos2)) {	
+								if ($pro2['val_puntuacio']==1){
+									echo"<i id=$pro0[mus_id] class=' thumbs outline down icon' onclick='suscriN($pro0[mus_id]);'></i>$pro0[totalvots] Te Gusta ";
+								} else {
+									echo"<i id=$pro1[mus_id] class=' thumbs outline up icon' onclick='suscriM($pro0[mus_id]);'></i>$pro0[totalvots] Ya no te gusta ";
+									
+								}
+							}
+						}
+						echo "</div></div></article></div>";
 					}
 					echo "</div>";
 				}
