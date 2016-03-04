fe6dc69e9e25ed3fdb28b38f1d5ae548a42b2013
sergioayalaj23 <39202.joan23@fje.edu>
2016-03-04 09:00:11 +0100

    borrar-canciones-funcional

--- procs/eliminar_cancion_propia.proc.php
+++ procs/eliminar_cancion_propia.proc.php
@@ -5,14 +5,17 @@
 	if($login == 1){
 		include("../conexion.php");
 		$mus_id=$_REQUEST['mus_id'];
-		echo $mus_id."<br/><br/>";
+		
+		$sql3="DELETE FROM `tbl_valoracio` WHERE `mus_id`=$mus_id";
+		$datos3 = mysqli_query($con, $sql3);
+		$sql2 = "DELETE FROM `tbl_llistes_musica` WHERE `mus_id`=$mus_id";
+		$datos2 = mysqli_query($con, $sql2);
 		$sql = "DELETE FROM `tbl_musica` WHERE `mus_id`=$mus_id";
 		$datos = mysqli_query($con, $sql);
-		echo $sql;
-		$sql2 = "DELETE FROM `tbl_llistes_musica` WHERE `mus_id`=$mus_id";
-		echo $sql2;
-		$datos2 = mysqli_query($con, $sql2);
-		//header("Location: ../verperfil.php");
+		
+
+		
+		header("Location: ../verperfil.php");
 	}else{
 		$_SESSION['validarse'] = 'error';
 		header("Location: ../login.php");
