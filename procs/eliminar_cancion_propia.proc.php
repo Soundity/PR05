ae4394fc8ad13685dfe2d4b346f17f155ce9eb96
carlos93263 <93263.joan23@fje.edu>
2016-03-04 11:55:20 +0100

    subida index de enric que no le va el gitjub

--- procs/eliminar_cancion_propia.proc.php
+++ procs/eliminar_cancion_propia.proc.php
@@ -4,11 +4,15 @@
 	if(isset($_COOKIE['Soundity']))$login = 1;
 	if($login == 1){
 		include("../conexion.php");
+		
 		$mus_id=$_REQUEST['mus_id'];
+
 		$sql3="DELETE FROM `tbl_valoracio` WHERE `mus_id`=$mus_id";
 		$datos3 = mysqli_query($con, $sql3);
+		
 		$sql2 = "DELETE FROM `tbl_llistes_musica` WHERE `mus_id`=$mus_id";
 		$datos2 = mysqli_query($con, $sql2);
+		
 		$sql = "DELETE FROM `tbl_musica` WHERE `mus_id`=$mus_id";
 		$datos = mysqli_query($con, $sql);
 		header("Location: ../verperfil.php");
