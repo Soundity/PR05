97efc34755b9b2103b8af878b295458f01c77007
carlos93263 <93263.joan23@fje.edu>
2016-03-11 12:25:28 +0100

    Added files via upload
    este archivo va, no como el de Ayala

--- procs/eliminar_cancion_lista.proc.php
+++ procs/eliminar_cancion_lista.proc.php
@@ -3,15 +3,11 @@
 	if(isset($_SESSION['id']))$login = 1;
 	if(isset($_COOKIE['Soundity']))$login = 1;
 	if($login == 1){
+		$lmu_id=$_REQUEST['lmu_id'];
 		include("../conexion.php");
-		
-		
-		
-		$sql = "DELETE FROM tbl_llistes_musica WHERE lmu_id=$lmu_id";
+		$sql = "DELETE FROM `tbl_llistes_musica` WHERE lmu_id=$lmu_id";
 		$datos = mysqli_query($con, $sql);
-		
-		
-		header("Location: ../lista.php");
+		header("Location: ../lista.php?idllista=$_REQUEST[idllista]");
 	}else{
 		$_SESSION['validarse'] = 'error';
 		header("Location: ../login.php");
